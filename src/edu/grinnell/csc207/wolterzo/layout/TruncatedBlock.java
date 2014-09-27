f0f67de6db2ee9c53328ed633ac2163cdfcd8d64
wolterzo <wolterzo@grinnell.edu>
2014-09-27 11:35:39 -0500

    implement TruncatedBlock class

---
+++ src/edu/grinnell/csc207/wolterzo/layout/TruncatedBlock.java
@@ -0,0 +1,65 @@
+package edu.grinnell.csc207.wolterzo.layout;
+
+public class TruncatedBlock
+    implements TextBlock
+{
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+  /**
+   * original TextBlock
+   */
+  TextBlock contents;
+
+  /**
+   * desired width
+   */
+  int newWidth;
+
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public TruncatedBlock(TextBlock tb, int width)
+  {
+    this.contents = tb;
+    this.newWidth = width;
+  } // TruncatedBlock(TextBlock, int)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+  
+
+  /**
+   * Get the ith row of the block.
+   */
+  public String row(int i)
+    throws Exception
+  {
+    if (i > this.height())
+      {
+        throw new Exception("Invalid row " + i);
+      } // if invalid row
+    else
+      {
+        String originalRow = this.contents.row(i);
+        return originalRow.substring(0, this.newWidth);
+      } // else
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block
+   */
+  public int height()
+  {
+    return this.contents.height();
+  } // height()
+
+  /**
+   * Determine how many columns are in the block
+   */
+  public int width()
+  {
+    return this.newWidth;
+  } // width()
+
+} // class TruncatedBlock
