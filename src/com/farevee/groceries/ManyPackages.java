4100a6724afe85fc00a61ae5acb93562a9968e31
wolterzo <wolterzo@grinnell.edu>
2014-09-27 14:49:54 -0500

    implement ManyPackages class

--- src/com/farevee/groceries/ManyPackages.java
+++ src/com/farevee/groceries/ManyPackages.java
@@ -3,19 +3,55 @@ package com.farevee.groceries;
 public class ManyPackages
     implements Item
 {
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+  /**
+   * Type of package that there are many of.
+   */
+  Package type;
+  /**
+   * How many packages there are.
+   */
+  int count;
 
-  @Override
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public ManyPackages(Package type, int count)
+  {
+    this.type = type;
+    this.count = count;
+  } // ManyPackages(Package, int)
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+  
+  /**
+   * Determines the weight of the ManyPackages
+   */
   public Weight getWeight()
   {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    return new Weight(this.type.weight.unit(),
+                      (this.count * this.type.weight.amount()));
+  } // getWeight()
 
-  @Override
+  /**
+   * Determines the price of the ManyPackages
+   */
   public int getPrice()
   {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+    return this.count * this.type.price;
+  } // getPrice()
 
-}
+  /**
+   * Returns the String representation of ManyPackages
+   */
+  public String toString()
+  {
+    Package type = this.type;
+    return this.count + " x " + type.weight.toString() + " packages of "
+           + type.name;
+  } // toString()
+  
+} // class ManyPackages
