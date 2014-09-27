757a71e4fd4c6e883182e34986f1d23b0afdfa6d
wolterzo <wolterzo@grinnell.edu>
2014-09-27 14:14:16 -0500

    add fields and constructor for BulkFood

--- src/com/farevee/groceries/BulkFood.java
+++ src/com/farevee/groceries/BulkFood.java
@@ -2,5 +2,41 @@ package com.farevee.groceries;
 
 public class BulkFood
 {
-
-}
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+  /**
+   * Name of the BulkFood
+   */
+  String name;
+  
+  /**
+   * Unit of the BulkFood
+   */
+  Units unit;
+  
+  /**
+   * Price of the BulkFood per unit of BulkFood
+   */
+  int pricePerUnit;
+  
+  /**
+   * Supply of BulkFood
+   */
+  int supply;
+  
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public BulkFood(String name, Units unit, int pricePerUnit, int supply)
+  {
+    this.name = name;
+    this.unit = unit;
+    this.pricePerUnit = pricePerUnit;
+    this.supply = supply;
+  } // BulkFood
+  
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+  
+} // class BulkFood
