616c296d2acbd961f5224001a5c7eaef78cb0815
wolterzo <wolterzo@grinnell.edu>
2014-09-27 14:14:54 -0500

    design/write Item interface

--- src/com/farevee/groceries/Item.java
+++ src/com/farevee/groceries/Item.java
@@ -1,6 +1,28 @@
 package com.farevee.groceries;
 
-public class Item
+/**
+ * Items in a grocery store
+ * @author Zoe G. Wolter
+ *
+ */
+public interface Item
 {
-
-}
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+  /**
+   * Determines the weight of the Item
+   */
+  public Weight getWeight();
+  
+  /**
+   * Determines the price of the Item
+   */
+  public int getPrice();
+  
+  /**
+   * Determined the string representation of the Item
+   */
+  public String toString();
+  
+} // interface Item
