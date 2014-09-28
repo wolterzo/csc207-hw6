ab74726734bfc56acc1597310474706ef669b16f
wolterzo <wolterzo@grinnell.edu>
2014-09-27 21:02:14 -0500

    add Cart class to shopping package. Write skeleton methods

---
+++ src/com/farevee/shopping/Cart.java
@@ -0,0 +1,65 @@
+package com.farevee.shopping;
+
+import java.io.PrintWriter;
+import java.util.LinkedList;
+
+import com.farevee.groceries.Item;
+import com.farevee.groceries.Weight;
+
+public class Cart
+{
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+  /**
+   * Linked list that contains the items in the cart.
+   */
+  LinkedList<Item> items;
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+ 
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+  
+  public void addItem(Item item)
+  {
+    
+  } // addItem(Item)
+  
+  public int numThings()
+  {
+    return 0;
+  } // numThings()
+  
+  public int numEntries()
+  {
+    return 0;
+  } // numEntries()
+  
+  public void printContents(PrintWriter pen)
+  {
+    
+  } // printContents(PrintWriter)
+  
+  public int getPrice()
+  {
+    return 0;
+  } // getPrice()
+  
+  public Weight[] getWeight()
+  {
+    return null;
+  } // getWeight()
+  
+  public void remove(String name)
+  {
+    
+  } // remove(String)
+  
+  public void merge()
+  {
+    
+  } // merge()
+  
+} // class Cart
