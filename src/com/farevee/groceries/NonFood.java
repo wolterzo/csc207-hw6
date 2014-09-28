28411805b87aa6827a73cf6cbd236b1e42f2eb77
wolterzo <wolterzo@grinnell.edu>
2014-09-27 19:52:10 -0500

    implement NonFood class

--- src/com/farevee/groceries/NonFood.java
+++ src/com/farevee/groceries/NonFood.java
@@ -3,19 +3,57 @@ package com.farevee.groceries;
 public class NonFood
     implements Item
 {
-
-  @Override
+  // +--------+------------------------------------------------------------
+  // | Fields |
+  // +--------+
+  /**
+   * Name of the NonFood item 
+   */
+  String name;
+  /**
+   * Weight of the NonFood item
+   */
+  Weight weight;
+  /**
+   * Price of the NonFood item
+   */
+  int price;
+  
+  // +--------------+------------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public NonFood(String name, Weight weight, int price)
+  {
+    this.name = name;
+    this.weight = weight;
+    this.price = price;
+  } // NonFodd(String, Weight, int)
+  
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+  
+  
+  /**
+   * Determines the weight of the item
+   */
   public Weight getWeight()
   {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    return this.weight;
+  } // getWeight()
 
-  @Override
+  /**
+   * Determines the price of the item
+   */
   public int getPrice()
   {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-}
+    return this.price;
+  } // getPrice()
+  
+  /**
+   * Returns a string representation of the item
+   */
+  public String toString()
+  {
+    return this.name;
+  } // toString()
+} // class NonFood
